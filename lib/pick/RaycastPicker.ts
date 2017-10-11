87d0cbf90e9abe37e806fa8f3231c11cc08fdd12
80prozent <80prozent@differentdesign.de>
2017-10-11 17:48:42 +0200

    minor update for RaycastPicker

--- lib/pick/RaycastPicker.ts
+++ lib/pick/RaycastPicker.ts
@@ -122,6 +122,7 @@ export class RaycastPicker extends TraverserBase implements IPicker
 
 	private sortOnNearT(entity1:IEntity, entity2:IEntity):number
 	{
+		//return entity1._iPickingCollision.rayEntryDistance > entity2._iPickingCollision.rayEntryDistance? 1 : -1;// use this for Icycle;
 		return entity1._iPickingCollision.rayEntryDistance > entity2._iPickingCollision.rayEntryDistance? 1 : entity1._iPickingCollision.rayEntryDistance < entity2._iPickingCollision.rayEntryDistance?-1:0;
 	}
 
