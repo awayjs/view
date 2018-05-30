b3ee1013a54bc94710036dbf59f06aa08a759ef2
80prozent <80prozent@differentdesign.de>
2018-05-30 10:06:17 +0200

    log version to console

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+console.log("AwayJS - View - 0.3.27");
 export {MouseManager} from "./lib/managers/MouseManager";
 export {ViewImage2D} from "./lib/image/ViewImage2D";
 export {BasicPartition} from "./lib/partition/BasicPartition";
