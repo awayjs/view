337071cbf1dc0e611071120cecd9018e614ec734
Rob Bateman <rob@infiniteturtles.co.uk>
2018-06-12 17:44:25 +0100

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.log("AwayJS - View - 0.3.31");
+console.log("AwayJS - View - 0.3.32");
 export {MouseManager} from "./lib/managers/MouseManager";
 export {ViewImage2D} from "./lib/image/ViewImage2D";
 export {BasicPartition} from "./lib/partition/BasicPartition";
