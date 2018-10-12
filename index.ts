1427974cc181a2bba0ff0af1cc01df78197a20d7
80prozent <80prozent@differentdesign.de>
2018-10-12 10:20:39 +0200

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.log("AwayJS - View - 0.3.53");
+console.log("AwayJS - View - 0.3.54");
 export {MouseManager} from "./lib/managers/MouseManager";
 export {ViewImage2D} from "./lib/image/ViewImage2D";
 
