70d87d37b73c03f5bcbf818849a0fab390e6edc5
80prozent <80prozent@differentdesign.de>
2019-02-28 15:55:27 +0100

    remove event.preventDefault() from MouseManager

--- lib/managers/MouseManager.ts
+++ lib/managers/MouseManager.ts
@@ -658,7 +658,7 @@ export class MouseManager {
     // ---------------------------------------------------------------------
 
     public onKeyDown(event): void {
-        event.preventDefault();
+        //event.preventDefault();
 
         if (this._currentFocusEntity || this._stage) {
             //console.log("dispatch keydown on ", this._currentFocusEntity);
@@ -674,7 +674,7 @@ export class MouseManager {
 
     }
     public onKeyUp(event): void {
-        event.preventDefault();
+        //event.preventDefault();
 
         if (this._currentFocusEntity || this._stage) {
             //console.log("dispatch keydown on ", this._currentFocusEntity);
